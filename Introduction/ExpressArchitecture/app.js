84094d5748c3ea98e8bc1277d29a95ed60eb0b75
Praveen Nagaraj <praveennagaraj76@gmail.com>
2026-07-27 10:08:38 +0530

    first commit

---
+++ Introduction/ExpressArchitecture/app.js
@@ -0,0 +1,13 @@
+// Client sends request
+
+// Node.js HTTP Server receives request
+
+// Express Application receives request
+
+// Middleware executes
+
+// Router matches URL
+
+// Route Handler executes
+
+// Response sent back to client
---
+++ Introduction/RequestResponseCycle/app.js
@@ -0,0 +1,15 @@
+// Client sends request
+
+// Node.js receives request
+
+// Express application receives request
+
+// Middleware executes
+
+// Route is matched
+
+// Business logic executes
+
+// Response is sent
+
+// Request-Response Cycle completed
